ecccc81529133f5cbcc85a2a9c5fa7f818ce0925
Bartosz Wojtaś <el.klides@op.pl>
2018-01-26 22:02:01 +0100

    Change scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+var a = prompt('Enter length of the base of the triangle '),
+	h = prompt('Enter height of the triangle '),
+	triangleArea = a * h / 2;
+alert('Titangle arena is, ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
